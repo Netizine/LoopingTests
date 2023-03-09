f124a9a4f2129d3a693bd1e5912b44c412618f70
James <james@melvin.tech>
2023-03-09 01:32:11 +0000

    Add project files.

---
+++ LoopingTests/Benchmarks.cs
@@ -0,0 +1,89 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using BenchmarkDotNet.Attributes;
+using FakeItEasy;
+using LoopingTests;
+
+namespace LoopingTests;
+
+[MemoryDiagnoser(false)]
+public class Benchmarks
+{
+    [Params(100, 1000, 10000)]
+    public int Count { get; set; }
+
+    private static User[] Users;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        Users = Enumerable.Range(1, Count)
+            .Select(x => A.Fake<User>(y => y.WithArgumentsForConstructor(new object[] { $"Fake{x}", $"User{x}", $"fake{x}@faked.com" }))).ToArray();
+    }
+
+    [Benchmark]
+    public void NormalLoop()
+    {
+        for (int i = 0; i < Users.Length; i++)
+        {
+            var user = Users[i];
+            user.GetFirstName();
+        }
+    }
+
+    [Benchmark]
+    public void NormalLoopWithSpan()
+    {
+        var span = Users.AsSpan();
+        for (int i = 0; i < span.Length; i++)
+        {
+            var user = span[i];
+            user.GetFirstName();
+        }
+    }
+
+    [Benchmark]
+    public void ForEachLoop()
+    {
+        for (int i = 0; i < Users.Length; i++)
+        {
+            var user = Users[i];
+            user.GetFirstName();
+        }
+    }
+
+    [Benchmark]
+    public void ForEachLoopWithSpan()
+    {
+        var span = Users.AsSpan();
+        for (int i = 0; i < span.Length; i++)
+        {
+            var user = span[i];
+            user.GetFirstName();
+        }
+    }
+
+    [Benchmark]
+    public void FasterLoop()
+    {
+        ref var searchSpace = ref MemoryMarshal.GetArrayDataReference(Users);
+        for (int i = 0; i < Users.Length; i++)
+        {
+            var user = Unsafe.Add(ref searchSpace, i);
+            user.GetFirstName();
+        }
+    }
+
+    [Benchmark]
+    public void FastestLoop()
+    {
+        ref var start = ref MemoryMarshal.GetArrayDataReference(Users);
+        ref var end = ref Unsafe.Add(ref start, Users.Length);
+
+        while (Unsafe.IsAddressLessThan(ref start, ref end))
+        {
+            start.GetFirstName();
+            start = ref Unsafe.Add(ref start, 1);
+        }
+    }
+}
---
+++ LoopingTests/User.cs
@@ -0,0 +1,53 @@
+﻿namespace LoopingTests
+{
+    public class User
+    {
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+
+        private User()
+        {
+
+        }
+        public User(string firstName, string lastName, string email)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _email = email;
+        }
+        public string FirstName
+        {
+            get => _firstName;
+            set => _firstName = value;
+        }
+
+        public string LastName
+        {
+            get => _lastName;
+            set => _lastName = value;
+        }
+
+        public string Email
+        {
+            get => _email;
+            set => _email = value;
+        }
+
+        public string GetFirstName()
+        {
+            return _firstName;
+        }
+
+        public string GetLastName()
+        {
+            return _firstName;
+        }
+
+        public string GetEmail()
+        {
+            return _email;
+        }
+    }
+
+}
